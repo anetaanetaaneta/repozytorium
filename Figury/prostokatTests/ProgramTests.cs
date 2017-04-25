16deb55c07cf968f677099137ce4ab371dac5346
Aneta Pieta <pietaaneta@gmail.com>
2017-04-25 13:56:25 +0200

    test1 failed prostokat

---
+++ Figury/prostokatTests/ProgramTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Figury;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figury.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void prostokatTest()
+        {
+            Program.prostokat(2, 3, 4, 5);
+            Assert.Fail();
+        }
+    }
+}
